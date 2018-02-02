13e3765a3568a6d3f8067e7469452b4e245dcd8e
guwei <349177019@qq.com>
2018-02-02 16:59:48 +0800

    delete redundant files

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,19 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry:{
-    index: './src/index.js'
+    jam: './src/index.js'
   },
   output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
   },
   plugins: [
-        new CleanWebpackPlugin('dist')
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // })
+        new CleanWebpackPlugin('dist'),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
         // new webpack.optimize.CommonsChunkPlugin({
         //     children: true,
         //     minChunks: Infinity,

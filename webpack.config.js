fe40b8380bbfe47ffb310790c5c8189db941a067
guwei <349177019@qq.com>
2018-02-06 18:26:18 +0800

    打包压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,65 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const uglify = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry:{
     jam: './src/index.js'
   },
   output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+      filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-        new CleanWebpackPlugin('dist'),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader'
+                }]
             }
-        })
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     children: true,
-        //     minChunks: Infinity,
-        //     async: false
-        // })
-        // new webpack.optimize.CommonsChunkPlugin({
-        //   name: "vendor",
-        //   minChunks: function(module){
-        //     return module.context && module.context.includes("node_modules");
-        //   }
-        // }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //   name: "manifest",
-        //   minChunks: Infinity
-        // }),
+        ]
+    }, 
+  plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+          compress: {
+              warnings: false,
+              comparisons: false
+          },
+          ecma: 5,
+          mangle: false,
+          output: {
+          },
+          sourceMap: true
+      }),
+    //   new webpack.optimize.CommonsChunkPlugin({
+    //       name: 'manifest',
+    //       chunks: ['vendor']
+    //   })
+        new CleanWebpackPlugin('dist'),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+        // new uglify
+//         // new webpack.optimize.CommonsChunkPlugin({
+//         //     children: true,
+//         //     minChunks: Infinity,
+//         //     async: false
+//         // })
+//         // new webpack.optimize.CommonsChunkPlugin({
+//         //   name: "vendor",
+//         //   minChunks: function(module){
+//         //     return module.context && module.context.includes("node_modules");
+//         //   }
+//         // }),
+//         // new webpack.optimize.CommonsChunkPlugin({
+//         //   name: "manifest",
+//         //   minChunks: Infinity
+//         // }),
     ]
 };
